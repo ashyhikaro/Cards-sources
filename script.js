e0b28a8622b2805fd7767b6f69e9370907a5e87e
Oleh Marynchuk <happyhikaro@gmail.com>
2021-10-24 14:28:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActive()
+        
+        slide.classList.add('active')
+    })
+}
+
+clearActive = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
